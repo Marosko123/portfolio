3cc738fd3b17e39195fdd057fa88de6abdc720eb
maros.bednar <bednarmaros341@gmail.com>
2024-05-24 22:26:39 +0200

    New deployment

--- src/common/keyboard-characters.js
+++
@@ -1,8 +0,0 @@
-export class KeyboardCharacters {
-    static letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-    static lowerCaseLetters = KeyboardCharacters.letters.toLowerCase()
-    static numbers = '0123456789'
-    static lettersAndNumbers =
-        KeyboardCharacters.letters + KeyboardCharacters.numbers
-    static specialCharacters = '!@#$%^&*()_+{}:"<>?|[];\',./`~'
-}
